25e9ffcc8d9445b937a1dd3ab0643bd5ec4fc689
pan <13329870472@163.com>
2025-08-14 18:02:58 +0800

    perf: img

--- src/components/ReCropperPreview/src/index.vue
+++ src/components/ReCropperPreview/src/index.vue
@@ -1,83 +1,94 @@
 <script setup lang="tsx">
-import { ref } from "vue";
-import ReCropper from "@/components/ReCropper";
+import { ref, onMounted } from "vue";
+import ImgCutter from "vue-img-cutter";
 import { formatBytes } from "@pureadmin/utils";
 
 defineOptions({
   name: "ReCropperPreview"
 });
 
-defineProps({
-  imgSrc: String
-});
+const props = defineProps<{ imgSrc: string }>();
 
 const emit = defineEmits(["cropper"]);
-
-const infos = ref();
-const popoverRef = ref();
-const refCropper = ref();
-const showPopover = ref(false);
-const cropperImg = ref<string>("");
-
-function onCropper({ base64, blob, info }) {
-  infos.value = info;
-  cropperImg.value = base64;
-  emit("cropper", { base64, blob, info });
+interface Info {
+  width?: string;
+  height?: string;
+  size?: number;
 }
+const infos = ref<Info>({
+  width: "0",
+  height: "0",
+  size: 0
+});
+const popoverRef = ref();
+const showPopover = ref(false);
+const childComponentRef = ref(null);
 
 function hidePopover() {
   popoverRef.value.hide();
 }
+function forIe9() {
+  childComponentRef.value?.handleOpen({
+    name: "1.png",
+    src: props.imgSrc
+  });
+}
+function imageLoadComplete(ref) {
+  infos.value.width = ref?.width;
+  infos.value.height = ref?.height;
+}
+function cutDown(data) {
+  console.log(data);
 
+  emit("cropper", data);
+  hidePopover();
+}
+function handlePrintImg(data) {
+  infos.value.size = data?.file?.size;
+  console.log("打印图片", data);
+}
+onMounted(() => {
+  // 组件加载完成后执行
+  forIe9(); // 例如自动打开裁剪远程图片
+});
 defineExpose({ hidePopover });
 </script>
 
 <template>
-  <div v-loading="!showPopover" element-loading-background="transparent">
-    <el-popover
-      ref="popoverRef"
-      :visible="showPopover"
-      placement="right"
-      width="18vw"
-    >
-      <template #reference>
-        <el-row>
-          <el-col :span="12">
-            <div class="w-[18vw]">
-              <ReCropper
-                ref="refCropper"
-                :src="imgSrc"
-                circled
-                @cropper="onCropper"
-                @readied="showPopover = true"
-              />
-              <p v-show="showPopover" class="mt-1 text-center">
-                温馨提示：右键上方裁剪区可开启功能菜单
-              </p>
-            </div>
-          </el-col>
-          <el-col :span="12">
-            <div class="w-[18vw] justify-center items-center text-center">
-              <el-image
-                v-if="cropperImg"
-                :src="cropperImg"
-                :preview-src-list="Array.of(cropperImg)"
-                fit="cover"
-              />
-              <div v-if="infos" class="mt-1">
-                <p>
-                  图像大小：{{ parseInt(infos.width) }} ×
-                  {{ parseInt(infos.height) }}像素
-                </p>
-                <p>
-                  文件大小：{{ formatBytes(infos.size) }}（{{ infos.size }}
-                  字节）
-                </p>
-              </div>
-            </div>
-          </el-col>
-        </el-row>
-      </template>
-    </el-popover>
-  </div>
+  <el-row>
+    <el-col :span="12">
+      <div class="w-[18vw]">
+        <ImgCutter
+          ref="childComponentRef"
+          :isModal="false"
+          :boxWidth="373"
+          @onImageLoadComplete="imageLoadComplete"
+          @cutDown="cutDown"
+          @onPrintImg="handlePrintImg"
+        />
+        <p v-show="showPopover" class="mt-1 text-center">
+          温馨提示：右键上方裁剪区可开启功能菜单
+        </p>
+      </div>
+    </el-col>
+    <el-col :span="12">
+      <div class="w-[18vw] justify-center items-center text-center">
+        <el-image
+          :src="props.imgSrc"
+          toolBgc="#fff"
+          :preview-src-list="Array.of(props.imgSrc)"
+          fit="cover"
+        />
+        <div class="mt-1">
+          <p>
+            图像大小：{{ parseInt(infos.width) }} ×
+            {{ parseInt(infos.height) }}像素
+          </p>
+          <p>
+            文件大小：{{ formatBytes(infos.size) }}（{{ infos.size }} 字节）
+          </p>
+        </div>
+      </div>
+    </el-col>
+  </el-row>
 </template>
